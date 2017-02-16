f43be4e9fdb25b431cf8bb51d523e9a09b565839
barryrowelge <barry.rowe@lge-ku.com>
2017-02-16 13:44:17 -0500

    Provide internal dependencies via provider Factory
    This resolves issue #1
    
    The providers are setup using a factory now, that clearly sets the
    dependency tokens via 'deps' property.

--- src/app/ngx-dev-utils.module.ts
+++ src/app/ngx-dev-utils.module.ts
@@ -6,6 +6,10 @@ import { PrettyObjectComponent } from './components/pretty-object.component';
 import { LogService } from './services/log.service';
 import { TestService } from './services/test.service';
 
+export function testServiceFactory(logService: LogService): TestService{
+  return new TestService(logService);
+}
+
 @NgModule({
   declarations: [
     PrettyObjectComponent
@@ -16,7 +20,10 @@ import { TestService } from './services/test.service';
   exports:[
     PrettyObjectComponent
   ],
-  providers: [LogService, TestService],
+  providers: [
+    LogService,
+    {provide: TestService, useFactory: testServiceFactory, deps:[LogService] }
+  ],
   bootstrap: []
 })
 export class NgxDevUtilsModule { }
